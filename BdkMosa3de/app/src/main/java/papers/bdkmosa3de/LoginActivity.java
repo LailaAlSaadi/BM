52f0118bacc72e31ad8cd957695fb71589786ebb
LailaAlSaadi <alsaadiilaila@gmail.com>
2018-06-22 03:11:41 +0300

    ui splash login sign up screen

---
+++ BdkMosa3de/app/src/main/java/papers/bdkmosa3de/LoginActivity.java
@@ -0,0 +1,24 @@
+package papers.bdkmosa3de;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends AppCompatActivity {
+
+    @AfterViews
+    public void hideKeyboard() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+    public void goToSignUp(View view) {
+        Intent i = new Intent(LoginActivity.this, SignUpActivity_.class);
+        startActivity(i);
+
+    }
+
+}
---
+++ BdkMosa3de/app/src/main/java/papers/bdkmosa3de/SignUpActivity.java
@@ -0,0 +1,20 @@
+package papers.bdkmosa3de;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_signup)
+public class SignUpActivity extends AppCompatActivity {
+
+    @AfterViews
+    public void hideKeyboard() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+
+}
